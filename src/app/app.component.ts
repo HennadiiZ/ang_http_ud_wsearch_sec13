5359a5b0beaec82ee33705163b6f91384f2a7911
Hennadii <82006228+HennadiiZ@users.noreply.github.com>
2022-06-01 10:40:15 +0300

    SearchBar + PageList components + wikipedia service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  
+
+  //Section 13: Handling Data and HTTP Requests
+  // SearchBar + PageList components + wikipedia service
 }
