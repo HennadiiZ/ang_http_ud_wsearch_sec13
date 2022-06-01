f8b218cba2ea6d146b7cb6fb1b7b1e7f45029b1d
Hennadii <82006228+HennadiiZ@users.noreply.github.com>
2022-06-01 12:30:18 +0300

    just some services explanation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { WikipediaService } from './wikipedia.service';
 
 @Component({
   selector: 'app-root',
@@ -7,15 +8,21 @@ import { Component, Input } from '@angular/core';
 })
 export class AppComponent {
 
-  getSearchValue = '';
+  constructor(private wikipediaService: WikipediaService){}
 
   onTerm(term: any){
-    this.getSearchValue = term
-      console.log("this is ", term)
-      console.log("this is +++", this.getSearchValue)
+      const res = this.wikipediaService.search(term)
+      console.log(res)
   }
 
   //Section 13: Handling Data and HTTP Requests
   // SearchBar + PageList components + wikipedia service
   // Child to Parent Communication
+
+  //https://en.wikipedia.org/w/api.php?
+            // action=query&
+            // format=json&
+            // list=search&
+            // utf8=1&
+            //srsearch=space
 }
